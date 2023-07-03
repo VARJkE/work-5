ed8732c67f8f6b5f70b53807236b4588dc46e4a0
Lado Chopinovi <lado.chopinovi@gmail.com>
2023-07-03 20:55:04 +0400

    commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const months = ['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი'];
 let divId = 0
 
+let tasksArray = [];
+
 function addTask() {
     divId++;
     let taskName = document.getElementById('taskName').value;
@@ -15,28 +17,43 @@ function addTask() {
     buttonRemove.setAttribute('class', 'button');
     buttonRemove.setAttribute('onclick', 'removeTask(this)');
     buttonRemove.textContent = 'Delete';
+
+    let removeTask = document.createElement('img');
+    removeTask.src = 'img/remove.png';
+    removeTask.className = 'icon';
+    removeTask.addEventListener('click', function() {
+        document.getElementById(newTask.id).remove();
+        removeTask.removeEventListener('click');
+    });
+
     newTask.innerHTML = '<h3>'+taskName+'</h3> <p>'+taskDesc+'</p> <p>'+taskAssign+'</p> <p>'+taskAddTime+'</p> <p>'+taskStatus+'</p>';
     document.getElementById('tasksList').classList.add('border-bottom');
+    
     switch (taskStatus) {
         case 'toDo':
             newTask.style.backgroundColor = '#e5eb42';
             document.getElementById('tasksToDo').appendChild(newTask);
             document.getElementById(newTask.id).appendChild(buttonRemove);
+            document.getElementById(newTask.id).appendChild(removeTask);
+
             break;
         case 'inProgress':
             newTask.style.backgroundColor = '#48e8a3';
             document.getElementById('tasksInProgress').appendChild(newTask);
             document.getElementById(newTask.id).appendChild(buttonRemove);
+            document.getElementById(newTask.id).appendChild(removeTask);
             break;    
         case 'testing':
             newTask.style.backgroundColor = '#8a8c88';
             document.getElementById('tasksTesting').appendChild(newTask);
             document.getElementById(newTask.id).appendChild(buttonRemove);
+            document.getElementById(newTask.id).appendChild(removeTask);
             break;    
         case 'done':
             newTask.style.backgroundColor = '#45d96d';
             document.getElementById('tasksDone').appendChild(newTask);
             document.getElementById(newTask.id).appendChild(buttonRemove);
+            document.getElementById(newTask.id).appendChild(removeTask);
             break;
         default:
             console.log('არასწორი მნიშნველობა');       
